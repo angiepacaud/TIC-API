5d3e7ee8b4555348cd38a73b927244331f57e3ba
angiepacaud <angie.pacaud@gmail.com>
2017-12-28 22:04:34 +0100

    domain_requete

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../views/queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/api/domains', db.getAllDomain);
 
 module.exports = router;
--- myapp/views/queries.js
+++ myapp/views/queries.js
@@ -7,13 +7,33 @@ var connection = mysql.createConnection({
   database : 'TIC-REST'
 });
 
-// connection.connect();
+connection.connect();
 
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
-//   console.log('The solution is: ', rows[0].solution);
-// });
+connection.query(function(err, rows) {
+  if (err) throw err;
+  console.log('ok');
+});
 
-// connection.end();
+connection.end();
 
-//
+// add query functions
+
+function getAllDomain(req, res, next) {
+  db.any('select * from domain__domain')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL users.'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+	getAllDomain:getAllDomain
+	
+};
